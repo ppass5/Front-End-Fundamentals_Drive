b81207edc1a19796e078dee4ae62c45146b9ea1a
ppass5 <lena.m.gonzalez@gmail.com>
2018-03-21 09:48:49 -0400

    saved some stuff

---
+++ css_animation/js/click.js
@@ -0,0 +1,64 @@
+
+$("#munchkin1").on("click", function() {
+  tl = TweenMax;
+  tl.to("#munchkin1", 1, {
+    rotation: 360,
+    // width: 200
+  })
+});
+
+$("#munchkin2").on("click", function() {
+  tl = TweenMax;
+  tl.to("#munchkin2", 1, {
+    rotation: 360,
+    // width: 200
+  })
+});
+
+$("#munchkin3").on("click", function() {
+  tl = TweenMax;
+  tl.to("#munchkin3", 1, {
+    rotation: 360,
+    // width: 200
+  })
+});
+
+$("#munchkin4").on("click", function() {
+  tl = TweenMax;
+  tl.to("#munchkin4", 1, {
+    rotation: 360,
+    // width: 200
+  })
+});
+
+$("#munchkin5").on("click", function() {
+  tl = TweenMax;
+  tl.to("#munchkin5", 1, {
+    rotation: 360,
+    // width: 200
+  })
+});
+
+$("#munchkin6").on("click", function() {
+  tl = TweenMax;
+  tl.to("#munchkin6", 1, {
+    rotation: 360,
+    // width: 200
+  })
+});
+
+$("#munchkin7").on("click", function() {
+  tl = TweenMax;
+  tl.to("#munchkin7", 1, {
+    rotation: 360,
+    // width: 200
+  })
+});
+
+$("#munchkin8").on("click", function() {
+  tl = TweenMax;
+  tl.to("#munchkin8", 1, {
+    rotation: 360,
+    // width: 200
+  })
+});
